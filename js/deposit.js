8d5cd0955813b63562ede0154fb1e9712a6f9f82
MdMehediHasan22 <mdmehedi3799@gmail.com>
2024-10-29 23:42:33 +0600

    updated isNaN condition alert

--- js/deposit.js
+++ js/deposit.js
@@ -5,6 +5,13 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
     const newDepositAmount = parseFloat(newDepositAmountString);
+
+    depositField.value = '';
+
+    if(isNaN(newDepositAmount)){
+        alert('please provide a valid number');
+        return;
+    }
     //step-3:get the current deposit total
     //for non-input(element other than input,textarea) use innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
@@ -13,7 +20,7 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const updateDepositTotal = previousDepositTotal+newDepositAmount;
 
     depositTotalElement.innerText = updateDepositTotal;
-    depositField.value = '';
+    
     
     // balance 
     const balanceTotalElement = document.getElementById('balance-total');
